07390b080364e60f3a8c96b0948393a1d4618da2
tracebackerror <shivam9172@gmail.com>
2018-06-17 01:51:11 +0530

    POST Implemented

--- rest.go
+++ rest.go
@@ -2,17 +2,26 @@ package main
 
 import "github.com/gin-gonic/gin"
 import "net/http"
-
+import "github.com/jinzhu/gorm"
+import _ "github.com/jinzhu/gorm/dialects/mysql"
 
 
 type ToDoTask struct {
+    gorm.Model
 	Task     string `form:"task" json:"task" binding:"required"`
 	Status string `form:"status" json:"status" binding:"required"`
 }
 
-
 func setupRest() *gin.Engine{
 	r := gin.Default()
+    db, err := gorm.Open("mysql", "tracebackerror:12345@/todo?charset=utf8&parseTime=True&loc=Local")
+    if err != nil {
+        panic("failed to connect database")
+    }
+    //defer db.Close()
+    db.AutoMigrate(&ToDoTask{})
+    
+    
 	r.GET("/rest/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Rest Service is Up & Running",
@@ -27,16 +36,22 @@ func setupRest() *gin.Engine{
         })
         taskRestRouter.GET("/:id", func(c *gin.Context) {
             id := c.Param("id")
+            
+            c.JSON(200, gin.H{
+			"message": "Implementing",
+            })
             c.String(http.StatusOK, "API For Single Todo: rest/task/"+id)
         })
         taskRestRouter.POST("/", func(c *gin.Context) {
             var jsonData ToDoTask
             if err := c.ShouldBindJSON( &jsonData); err == nil {
-                if jsonData.Task != "" && jsonData.Status != "" {
-                    c.JSON(http.StatusOK, gin.H{"status": "Task logged in"})
-                } else {
-                    c.JSON(http.StatusUnauthorized, gin.H{"status": "Task failed to add in database"})
-                }
+                //todotaskdata := ToDoTask{Task: string(jsonData.Task), Status: string(jsonData.Status)}
+                
+                db.Create(&jsonData)
+                
+                message := "Task Logged"
+                c.JSON(http.StatusOK, gin.H{"message": message, ""})
+                
             } else {
                 c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
             }
