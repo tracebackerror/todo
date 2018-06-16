e7f90de3497e93216228283265da83118c06e5bd
tracebackerror <shivam9172@gmail.com>
2018-06-17 00:41:56 +0530

    Refactored code for fixing Gin Undefined ShouldBindJson

--- rest.go
+++ rest.go
@@ -3,36 +3,57 @@ package main
 import "github.com/gin-gonic/gin"
 import "net/http"
 
+
+
+type ToDoTask struct {
+	Task     string `form:"task" json:"task" binding:"required"`
+	Status string `form:"status" json:"status" binding:"required"`
+}
+
+
 func setupRest() *gin.Engine{
-	router := gin.Default()
-	router.GET("/rest/ping", func(c *gin.Context) {
+	r := gin.Default()
+	r.GET("/rest/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Rest Service is Up & Running",
 		})
 	})
     
-    
-    taskRestRouter := router.Group("rest/task")
-	{
+
+    taskRestRouter := r.Group("rest/task")
+    {
         taskRestRouter.GET("/", func(c *gin.Context) {
             c.String(http.StatusOK, "API For All List: rest/task/")
         })
         taskRestRouter.GET("/:id", func(c *gin.Context) {
-            c.String(http.StatusOK, "API For Single Todo: rest/task/1")
+            id := c.Param("id")
+            c.String(http.StatusOK, "API For Single Todo: rest/task/"+id)
         })
         taskRestRouter.POST("/", func(c *gin.Context) {
-            c.String(http.StatusOK, "API For Create Single Todo: rest/task/")
+            var jsonData ToDoTask
+            if err := c.ShouldBindJSON( &jsonData); err == nil {
+                if jsonData.Task != "" && jsonData.Status != "" {
+                    c.JSON(http.StatusOK, gin.H{"status": "Task logged in"})
+                } else {
+                    c.JSON(http.StatusUnauthorized, gin.H{"status": "Task failed to add in database"})
+                }
+            } else {
+                c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            }
+            
         })
         taskRestRouter.PUT("/:id", func(c *gin.Context) {
-            c.String(http.StatusOK, "API For Update Single Todo: rest/task/1")
+            id := c.Param("id")
+            c.String(http.StatusOK, "API For Update Single Todo: rest/task/"+id)
         })
         taskRestRouter.DELETE("/:id", func(c *gin.Context) {
             c.String(http.StatusOK, "API For DELETE Single Todo: rest/task/1")
         })
-
-     
+        
 	}
     
+   
     
-	return router
+    
+	return r
 }
