9942f3bcffa9ed243376ecae73cb6aef70718e38
tracebackerror <shivam9172@gmail.com>
2018-06-16 23:04:52 +0530

    Scaff Folding Created For ToDo

--- rest.go
+++ rest.go
@@ -3,14 +3,36 @@ package main
 import "github.com/gin-gonic/gin"
 import "net/http"
 
-func main() {
-	r := gin.Default()
-	r.GET("/rest/ping", func(c *gin.Context) {
+func setupRest() *gin.Engine{
+	router := gin.Default()
+	router.GET("/rest/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "Rest Service is Up & Running",
 		})
 	})
     
     
-	r.Run() // listen and serve on 0.0.0.0:8080
+    taskRestRouter := router.Group("rest/task")
+	{
+        taskRestRouter.GET("/", func(c *gin.Context) {
+            c.String(http.StatusOK, "API For All List: rest/task/")
+        })
+        taskRestRouter.GET("/:id", func(c *gin.Context) {
+            c.String(http.StatusOK, "API For Single Todo: rest/task/1")
+        })
+        taskRestRouter.POST("/", func(c *gin.Context) {
+            c.String(http.StatusOK, "API For Create Single Todo: rest/task/")
+        })
+        taskRestRouter.PUT("/:id", func(c *gin.Context) {
+            c.String(http.StatusOK, "API For Update Single Todo: rest/task/1")
+        })
+        taskRestRouter.DELETE("/:id", func(c *gin.Context) {
+            c.String(http.StatusOK, "API For DELETE Single Todo: rest/task/1")
+        })
+
+     
+	}
+    
+    
+	return router
 }
